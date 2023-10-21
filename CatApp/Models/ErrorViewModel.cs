09d18a74b16abb82bf2de2dfef7f6c998767e75f
zzzzDenzzzz <12den24@gmail.com>
2023-10-21 11:46:05 +0300

    Добавьте файлы проекта.

---
+++ CatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
